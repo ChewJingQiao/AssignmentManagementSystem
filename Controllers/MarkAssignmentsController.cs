c08ec960b8c7c1709b34b65190123e9d8c218d1e
CHEW JING QIAO <TP057785@mail.apu.edu.my>
2022-07-23 19:26:14 +0800

    filter lecturer assignment list

--- Controllers/MarkAssignmentsController.cs
+++ Controllers/MarkAssignmentsController.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using AssignmentManagementSystem.Models;
 using AssignmentManagementSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using AssignmentManagementSystem.Areas.Identity.Data;
+
 
 namespace AssignmentManagementSystem.Controllers
 {
     public class MarkAssignmentsController : Controller
     {
         private readonly AssignmentManagementSystemContext _context;
+        UserManager<AssignmentManagementSystemUser> UserManager;
+        SignInManager<AssignmentManagementSystemUser> SignInManager;
 
         public MarkAssignmentsController(AssignmentManagementSystemContext context)
         {
@@ -20,7 +25,54 @@ namespace AssignmentManagementSystem.Controllers
         public async Task<IActionResult> Index(string msg = "")
         {
             ViewBag.msg = msg;
-            return View(await _context.TeamAssignment.ToListAsync());
+            List<TeamAssignment> teamassignmentlist = await _context.TeamAssignment.ToListAsync();
+            List<LecturerModule> lecturermodulelist = await _context.LecturerModule.ToListAsync();
+            List<Assignment> assignmentlist = await _context.Assignment.ToListAsync();
+            string username = User.Identity.Name;
+            var userlist = this._context.User.ToList();
+            foreach(AssignmentManagementSystemUser ee in userlist)
+            {
+                if(ee.UserName == username)
+                {
+                    string useridnow = ee.Id;
+
+                    List<string> moduleofuser = new List<string>();
+                    List<string> assignmentofuser = new List<string>();
+                    List<TeamAssignment> finallist = new List<TeamAssignment>();
+                    foreach (LecturerModule lml in lecturermodulelist)
+                    {
+                        if (lml.UserId == useridnow)
+                        {
+                            moduleofuser.Add(lml.ModuleId);
+                        }
+                    }
+                    foreach (Assignment a in assignmentlist)
+                    {
+                        foreach (string b in moduleofuser)
+                        {
+                            if (a.ModuleRefId == b)
+                            {
+                                assignmentofuser.Add(a.AssignmentId);
+                            }
+                        }
+                    }
+                    foreach (TeamAssignment tal in teamassignmentlist)
+                    {
+                        foreach (string c in assignmentofuser)
+                        {
+                            if (tal.AssignmentId == c)
+                            {
+                                finallist.Add(tal);
+                            }
+                        }
+                    }
+                    return View(finallist);
+                }
+
+            }
+
+            return null;
+            //return View(await _context.TeamAssignment.ToListAsync());
         }
 
         public async Task<IActionResult> Mark(int?
